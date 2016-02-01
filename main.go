291f162d4635647965066d29bc5a9c0f6989830b
Tobi Lehman <mail@tobilehman.com>
2016-01-31 23:20:17 -0800

    Add output buffering (2x speedup)

--- main.go
+++ main.go
@@ -1,22 +1,37 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"os"
 )
 
 func main() {
 	var inputFileName string
+	var outputFileName string
+	var outputWriter io.Writer
 
 	flag.StringVar(&inputFileName, "i", "", "Input file of redis commands")
+	flag.StringVar(&outputFileName, "o", "", "Output file in redis protocol format")
 	flag.Parse()
 
-	file, err := os.Open(inputFileName)
-	defer file.Close()
-	if err == nil {
-		EncodeStream(file, os.Stdout)
+	inputFile, ierr := os.Open(inputFileName)
+	outputFile, oerr := os.Create(outputFileName)
+	defer inputFile.Close()
+	defer outputFile.Close()
+
+	if ierr == nil {
+		if oerr == nil {
+			outputWriter = outputFile
+		} else {
+			outputWriter = os.Stdout
+		}
+		bufwriter := bufio.NewWriterSize(outputWriter, 4294967296)
+		EncodeStream(inputFile, bufwriter)
+		bufwriter.Flush()
 	} else {
-		fmt.Fprintf(os.Stderr, "%v", err)
+		fmt.Fprintf(os.Stderr, "input error: %\noutput error: %v", ierr, oerr)
 	}
 }
