5a8354abad66bc97f89aa325330dae2c0f42a08e
Tobi Lehman <mail@tobilehman.com>
2016-02-15 15:03:04 -0800

    Add benchmarks for EncodeStream

--- redis_protocol_test.go
+++ redis_protocol_test.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"testing"
+	"io"
+	"bytes"
+	"strings"
 )
 
 func TestEncodeUnquoted(t *testing.T) {
@@ -25,5 +28,23 @@ func TestEncodeSingleCharUnquoted(t *testing.T) {
 	expected := "*2\r\n$3\r\nGET\r\n$1\r\nA\r\n"
 	if encoded != expected {
 		t.Fatal("Encode should handle one char unquoted args")
+	}
+}
+
+
+func BenchmarkEncode(b *testing.B) {
+	var text string = `
+HSET foo 3 1.2
+HSET foo 4 3.0
+HSET foo 5 4.88
+HSET bar 3 3.4
+HSET foo 12 1.9
+HSET foo 3 3.12
+`
+	var raw io.Reader = strings.NewReader(text)
+	var buf bytes.Buffer
+
+	for i := 0; i < b.N; i++ {
+		EncodeStream(raw, &buf)
 	}
 }
